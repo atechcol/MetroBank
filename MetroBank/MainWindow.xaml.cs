0cabc45efb668ebe1dd14fbbe42ddce2e76d5d08
Alexander Götzsche Dougherty <zombiont@gmail.com>
2026-05-14 17:32:46 +0200

    the app

---
+++ MetroBank/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MahApps.Metro.Controls;
+
+using MetroBank;
+using System;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using BankAccount;
+
+namespace MetroBank
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MetroBank.ViewModels.OverviewViewModel AccountViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AccountViewModel = new ViewModels.OverviewViewModel();
+            AccountViewModel.Account = new DebitBankAccount();
+            this.DataContext = AccountViewModel;
+        }
+
+        private void DepositButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AccountViewModel.Deposit();
+            DepositFlyout.IsOpen = true;
+            DepositBox.Text = "";
+        }
+
+        private void WithdrawButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            bool successful = AccountViewModel.Withdraw();
+            if (successful)
+            {
+                WithdrawFlyout.IsOpen = true;
+            }
+            else
+            {
+                WithdrawBadFlyout.IsOpen = true;
+            }
+            
+            WithdrawBox.Text = "";
+        }
+
+        private void DepositBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            decimal result;
+            bool parsed = decimal.TryParse(DepositBox.Text, out result);
+            if (parsed)
+            {
+                AccountViewModel.DepositAmount = result;
+            }
+        }
+
+        private void WithdrawBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            decimal result;
+            bool parsed = decimal.TryParse(WithdrawBox.Text, out result);
+            if (parsed)
+            {
+                AccountViewModel.WithdrawAmount = result;
+            } 
+        }
+    }
+}
---
+++ MetroBank/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using PropertyChanged;
+
+using BankAccount;
+
+namespace MetroBank.ViewModels
+{
+    public class OverviewViewModel : BaseViewModel
+    {
+        public Account Account { get; set; }
+
+        private decimal _balance;
+        public decimal Balance
+        {
+            get 
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+        public string BalanceString
+        {
+            get
+            {
+                string currency = Account.BalanceData.Valuta.Kind.ToString();
+                return Balance.ToString() + " " + currency;
+            }
+        }
+
+        public decimal DepositAmount { get; set; }
+        public decimal WithdrawAmount { get; set; }
+
+        public OverviewViewModel()
+        {
+            this.Balance = 0.0M;
+            this.DepositAmount = 0.0M;
+            this.WithdrawAmount = 0.0M;
+        }
+
+        public bool Deposit()
+        {
+            Account.Desposit(this.DepositAmount);
+            this.Balance = Account.BalanceData.Balance;
+            return true;
+        }
+
+        public bool Withdraw()
+        {
+            decimal amount = Account.Withdraw(this.WithdrawAmount);
+            this.Balance = Account.BalanceData.Balance;
+
+            if (amount == -1.0M)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
